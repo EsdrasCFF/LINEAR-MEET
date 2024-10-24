244c8b87e18664ef384fdf23b075e559bab09dcb
Esdras Castro <ecff.dev@gmail.com>
2024-10-24 09:25:44 -0300

    feat: add Footer component

--- src/features/room/components/footer.tsx
+++ src/features/room/components/footer.tsx
@@ -1,17 +1,27 @@
-import { MicIcon, MicOffIcon, PhoneIcon, Tv2 } from 'lucide-react'
+import { MicIcon, MicOffIcon, PhoneIcon, PhoneOff, Tv2, Video, VideoOff } from 'lucide-react'
 
 import { ControlButton } from './control-button'
+import { MdTvOff } from 'react-icons/md'
 
 export function Footer() {
-  const icons = [{ icon: MicIcon }, { icon: MicOffIcon }, { icon: Tv2 }, { icon: PhoneIcon }]
+  const icons = [
+    { icon: MicIcon, disabledIcon: MicOffIcon },
+    { icon: Video, disabledIcon: VideoOff },
+    { icon: Tv2, disabledIcon: MdTvOff },
+    { icon: PhoneIcon, disabledIcon: PhoneOff },
+  ]
+
+  const date = new Date()
+  const hours = date.getHours().toString().padStart(2, '0')
+  const minutes = date.getMinutes().toString().padStart(2, '0')
 
   return (
-    <footer className="flex w-full items-center justify-center bg-customBackground">
-      <div className="bottom-5 grid w-full max-w-7xl grid-cols-3">
-        <span className="col-span-1 text-xl font-medium text-white">10:00</span>
-        <div className="flex">
+    <footer className="fixed bottom-2 flex w-full items-center justify-center bg-customBackground px-5 font-rubik">
+      <div className="bottom-5 grid w-full max-w-7xl grid-cols-3 items-center">
+        <span className="text-xl font-medium text-white">{hours + ':' + minutes}</span>
+        <div className="flex items-center justify-center gap-2">
           {icons.map((icon, index) => (
-            <ControlButton icon={icon.icon} key={String(index + 'ba')} />
+            <ControlButton icon={icon.icon} key={String(index + 'ba')} disabledIcon={icon.disabledIcon} />
           ))}
         </div>
         <div> </div>
