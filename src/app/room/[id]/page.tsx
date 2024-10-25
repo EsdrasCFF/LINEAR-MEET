a38008e154d100853268adee63e4477456165793
Esdras Castro <ecff.dev@gmail.com>
2024-10-24 23:55:35 -0300

    fix: apply prettier

--- src/app/room/[id]/page.tsx
+++ src/app/room/[id]/page.tsx
@@ -1,11 +1,17 @@
 'use client'
 
+import { Chat } from '@/features/room/components/chat'
 import { Footer } from '@/features/room/components/footer'
 
 export default function RoomPage({ params }: { params: { id: string } }) {
   return (
-    <div className="h-full w-full">
-      {params.id}
+    <div className="flex h-full w-full flex-col justify-between p-5">
+      <div className="flex h-full w-full">
+        <div className="h-full w-[80%]">Absurdo</div>
+        <div className="h-full w-[20%]">
+          <Chat />
+        </div>
+      </div>
       <Footer />
     </div>
   )
